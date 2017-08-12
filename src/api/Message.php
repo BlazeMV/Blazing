85e676623d96bb6e964c5e1ed8ed8ec8c0d4e0d4
BlazeMV <ad.adamdavid72@gmail.com>
2017-08-12 13:24:15 +0500

    property case fixed

--- src/api/Message.php
+++ src/api/Message.php
@@ -25,7 +25,7 @@ class Message{
     protected $chat;
     protected $sender;
     protected $SendDate;
-    protected $ForwardOriginFrom;
+    protected $ForwardOriginSender;
     protected $ForwardOriginChat;
     protected $ForwardOriginMessageId;
     protected $ForwardOriginDate;
@@ -57,37 +57,37 @@ class Message{
     protected $MigrateFromChatId;
     protected $PinnedMessage;
     protected $invoice;
-    protected $SuccessfulSayment;
+    protected $SuccessfulPayment;
     //endregion
     
     public function __construct(array $message){
         $this->message = $message;
-        $this->messageid = $this->message['message_id'];
-        $this->senddate = $this->message['date'];
+        $this->MessageId = $this->message['message_id'];
+        if (isset($this->message['text'])) {
+            $this->text = $this->message['text'];
+        }
         $this->chat = new Chat($this->message['chat']);
         if (isset($this->message['from'])) {
             $this->sender = new User($this->message['from']);
         }
-        if (isset($this->message['text'])) {
-            $this->text = $this->message['text'];
-        }
+        $this->Date = $this->message['date'];
         if (isset($this->message['forward_from'])) {
-            $this->sender = new User($this->message['forward_from']);
+            $this->ForwardOriginSender = new User($this->message['forward_from']);
         }
         if (isset($this->message['forward_from_chat'])) {
-            $this->forwardfromchat = new chat($this->message['forward_from_chat']);
+            $this->ForwardOriginChat = new chat($this->message['forward_from_chat']);
         }
         if (isset($this->message['forward_from_message_id'])) {
-            $this->forwardoriginmessage_id = $this->message['forward_from_message_id'];
+            $this->ForwardOriginMessageId = $this->message['forward_from_message_id'];
         }
         if (isset($this->message['forward_date'])) {
-            $this->forwardorigindate = $this->message['forward_date'];
+            $this->ForwardOriginDate = $this->message['forward_date'];
         }
         if (isset($this->message['reply_to_message'])) {
-            $this->replymessage = new Message($this->message['reply_to_message']);
+            $this->ReplyMessage = new Message($this->message['reply_to_message']);
         }
         if (isset($this->message['edit_date'])) {
-            $this->editdate = $this->message['edit_date'];
+            $this->EditDate = $this->message['edit_date'];
         }
         if (isset($this->message['entities'])) {
             foreach ($this->message['entities'] as $entity) {
@@ -116,10 +116,10 @@ class Message{
             $this->voice = new Voice($this->message['voice']);
         }
         if (isset($this->message['video_note'])) {
-            $this->videonote = new VideoNote($this->message['video_note']);
+            $this->VideoNote = new VideoNote($this->message['video_note']);
         }
         if (isset($this->message['new_chat_members'])) {
-            $this->newchatmembers = $this->message['new_chat_members'];
+            $this->NewChatMembers = $this->message['new_chat_members'];
         }
         if (isset($this->message['caption'])) {
             $this->caption = $this->message['caption'];
@@ -134,43 +134,43 @@ class Message{
             $this->venue = new Venue($this->message['venue']);
         }
         if (isset($this->message['new_chat_member'])) {
-            $this->newchatmember = new User($this->message['new_chat_member']);
+            $this->NewChatMember = new User($this->message['new_chat_member']);
         }
         if (isset($this->message['left_chat_member'])) {
-            $this->leftchatmember = new User($this->message['left_chat_member']);
+            $this->LeftChatMember = new User($this->message['left_chat_member']);
         }
         if (isset($this->message['new_chat_title'])) {
-            $this->newchattitle = $this->message['new_chat_title'];
+            $this->NewChatTitle = $this->message['new_chat_title'];
         }
         if (isset($this->message['new_chat_photo'])) {
-            $this->newchatphoto = $this->message['new_chat_photo'];
+            $this->NewChatPhoto = $this->message['new_chat_photo'];
         }
         if (isset($this->message['delete_chat_photo'])) {
-            $this->deletechatphoto = $this->message['delete_chat_photo'];
+            $this->DeleteChatPhoto = $this->message['delete_chat_photo'];
         }
         if (isset($this->message['group_chat_created'])) {
-            $this->groupchatcreated = $this->message['group_chat_created'];
+            $this->GroupChatCreated = $this->message['group_chat_created'];
         }
         if (isset($this->message['supergroup_chat_created'])) {
-            $this->supergroupchatcreated = $this->message['supergroup_chat_created'];
+            $this->SuperGroupChatCreated = $this->message['supergroup_chat_created'];
         }
         if (isset($this->message['channel_chat_created'])) {
-            $this->channelchatcreated = $this->message['channel_chat_created'];
+            $this->ChannelChatCreated = $this->message['channel_chat_created'];
         }
         if (isset($this->message['migrate_to_chat_id'])) {
-            $this->migratetochatid = $this->message['migrate_to_chat_id'];
+            $this->MigrateToChatId = $this->message['migrate_to_chat_id'];
         }
         if (isset($this->message['migrate_from_chat_id'])) {
-            $this->migratefromchatid = $this->message['migrate_from_chat_id'];
+            $this->MigrateFromChatId = $this->message['migrate_from_chat_id'];
         }
         if (isset($this->message['pinned_message'])) {
-            $this->pinnedmessage = new Message($this->message['pinned_message']);
+            $this->PinnedMessage = new Message($this->message['pinned_message']);
         }
         if (isset($this->message['invoice'])) {
             $this->invoice = new Invoice($this->message['invoice']);
         }
         if (isset($this->message['successful_payment'])) {
-            $this->successfulpayment = new SuccessfulPayment($this->message['successful_payment']);
+            $this->SuccessfulPayment = new SuccessfulPayment($this->message['successful_payment']);
         }
     }
     
--- src/api/User.php
+++ src/api/User.php
@@ -14,15 +14,15 @@ class User{
     public function __construct($user){
         $this->user = $user;
         $this->id = $this->user['id'];
-        $this->first_name = $this->user['first_name'];
+        $this->FirstName = $this->user['first_name'];
         if (isset($this->user['last_name'])){
-            $this->last_name = $this->user['last_name'];
+            $this->LastName = $this->user['last_name'];
         }
         if (isset($this->user['username'])){
             $this->username = $this->user['username'];
         }
         if (isset($this->user['language_code'])){
-            $this->language_code = $this->user['language_code'];
+            $this->LanguageCode = $this->user['language_code'];
         }
     }
     
